3a3e43182f87d53fdc5d4465e5a933ec9dbb12e4
Coffeeteer <tgrieshiemer@gmail.com>
2016-06-23 15:06:55 -0400

    added comments to CSS, HTML & JS, found load button library called 'lazy button', and more CSS goodness

--- assets/front.js
+++ assets/front.js
@@ -1,5 +1,13 @@
 $(document).ready( function() {
 
+	//for dropdown menu action
 	$('.dropdown-toggle').dropdown();
+
+	//Lazy Loader to load page on bottom
+	$(function(){
+    $('.lazy-wrapper').lazyLoader({
+        ajaxLoader: 'get_datas.php' // script that gets items (from a database for example)
+    });
+});
 	
 });
